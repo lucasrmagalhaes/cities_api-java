075ae654161f5759282da94abe7120cddb70b1a4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-11-03 20:42:16 -0300

    Pageable

--- cities-api/src/main/java/com/github/lucasrmagalhaes/citiesapi/CountryResource.java
+++ cities-api/src/main/java/com/github/lucasrmagalhaes/citiesapi/CountryResource.java
@@ -2,11 +2,11 @@ package com.github.lucasrmagalhaes.citiesapi;
 
 import com.github.lucasrmagalhaes.citiesapi.countries.Country;
 import com.github.lucasrmagalhaes.citiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
@@ -18,7 +18,7 @@ public class CountryResource {
     }
 
     @GetMapping
-    public List<Country> countries() {
-        return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+        return repository.findAll(page);
     }
 }
